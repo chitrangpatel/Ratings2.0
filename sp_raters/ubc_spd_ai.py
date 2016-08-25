81f565bd9114de482d040071ed3bef6a581c9c4f
Chitrang Patel <patelc14@lg-1r17-n04.guillimin.clumeq.ca>
2016-08-25 15:17:01 -0400

    changed paths to point to the correct AI and now using version clfsvm_SP_v2.pkl for single pulse AI.

--- sp_raters/ubc_spd_ai.py
+++ sp_raters/ubc_spd_ai.py
@@ -19,7 +19,7 @@ class ubc_spd_ai(base.BaseRater):
     long_name = "UBC spd AI"
     description = "compute the prediction from the classifier " \
                   "based on spd files."
-    version = 3
+    version = 4
     
     rat_cls = profile.ProfileClass()
 
